a18d5beaece0dcf61a4a545c9e2458eca3e8b639
Adrian Moreno <adrianmn90@gmail.com>
2023-02-26 21:03:36 +0100

    feat() map aligned to the left
    forest color darkened

--- src/config.js
+++ src/config.js
@@ -7,14 +7,14 @@ export default {
     BORDER_WIDTH: 3,
     MAP_WIDTH: 41,
     MAP_HEIGHT: 41,
-    PERLIN_CELL_RESOLUTION: 18,
+    PERLIN_CELL_RESOLUTION: 19,
     MAP_COLOR_RANGES: [
         { MAX: -0.4, COLOR: '#256299' }, // DEEP
         { MAX: -0.03, COLOR: '#2375b4' }, // MEDIUM
         { MAX: 0.02, COLOR: '#4699de' }, // SURFACE
         { MAX: 0.09, COLOR: '#ab976a' }, // SAND
-        { MAX: 0.2, COLOR: '#367147' }, // DARK LOW GRASS
-        { MAX: 0.4, COLOR: '#457950' }, // LIGHT HIGH GRASS
+        { MAX: 0.2, COLOR: '#457950' }, // LIGHT HIGH GRASS
+        { MAX: 0.4, COLOR: '#2d673e' }, // DARK LOW GRASS
         { MAX: 0.5, COLOR: '#514635' }, // ROCK
         { MAX: 1, COLOR: '#f4f0e8' }, // SNOW
     ],
@@ -26,25 +26,20 @@ export default {
     },
     getOutterBorderRect() {
         let mapSize = this.getMapSize()
+        const mapPos = this.getMapPosition()
         return {
-            x: this.CANVAS_WIDTH / 2
-                - mapSize.width / 2
-                - this.CELL_SEPARATION
-                - this.BORDER_WIDTH,
-            y: this.CANVAS_HEIGHT / 2
-                - mapSize.height / 2
-                - this.CELL_SEPARATION
-                - this.BORDER_WIDTH
-                - this.BORDER_HEADER,
+            x: mapPos.x - this.CELL_SEPARATION - this.BORDER_WIDTH,
+            y: mapPos.y - this.CELL_SEPARATION - this.BORDER_WIDTH - this.BORDER_HEADER,
             width: mapSize.width + this.CELL_SEPARATION * 2 + this.BORDER_WIDTH * 2,
             height: mapSize.height + this.CELL_SEPARATION * 2 + this.BORDER_WIDTH * 2 + this.BORDER_HEADER
         }
     },
     getInnerBorderRect() {
         let mapSize = this.getMapSize()
+        const mapPos = this.getMapPosition()
         return {
-            x: this.CANVAS_WIDTH / 2 - mapSize.width / 2 - this.CELL_SEPARATION,
-            y: this.CANVAS_HEIGHT / 2 - mapSize.height / 2 - this.CELL_SEPARATION,
+            x: mapPos.x - this.CELL_SEPARATION,
+            y: mapPos.y - this.CELL_SEPARATION,
             width: mapSize.width + this.CELL_SEPARATION * 2,
             height: mapSize.height + this.CELL_SEPARATION * 2
         }
@@ -61,8 +56,8 @@ export default {
     getMapPosition() {
         let mapSize = this.getMapSize()
         return {
-            x: this.CANVAS_WIDTH / 2 - mapSize.width / 2,
-            y: this.CANVAS_HEIGHT / 2 - mapSize.height / 2
+            x: this.CANVAS_WIDTH - mapSize.width - this.CELL_SEPARATION - this.BORDER_WIDTH,
+            y: this.BORDER_HEADER + this.CELL_SEPARATION
         }
     }
 }
